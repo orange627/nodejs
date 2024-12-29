32e7b2dbc19252f59541a3f7c3e172bbdd98cb0f
Orange <orange627@ymail.ne.jp>
2024-12-29 21:16:40 +0900

    mydb.dbをprivateを移行

--- routes/boards.js
+++ routes/boards.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-const sqlite3=require('sqlite3');
-const db=new sqlite3.Database('mydb.db');
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('./private/mydb.db');
 
 //一度の表示されるメッセージの数
-const pnum=30;
+const pnum = 30;
 
 //usersと連携
 //referencesがあるとon delete cascadeでusersのidが消えるとaccountIdも消える。
@@ -21,67 +21,67 @@ db.run(`
 `);
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.redirect('/boards/0');
+router.get('/', function (req, res, next) {
+    res.redirect('/boards/0');
 });
 
 //ログインしてなかったらログインページに飛ばす
-function check(req,res){
-    if(req.session.login==null){
-        console.log('セッション切れてるっぽいぜ:'+req.session.login);
-        req.session.back='/boards';
+function check(req, res) {
+    if (req.session.login == null) {
+        console.log('セッション切れてるっぽいぜ:' + req.session.login);
+        req.session.back = '/boards';
         res.redirect('/users/enter');
         return true;
-    }else{
+    } else {
         return false;
     }
 }
 //ユーザー全員のメッセージを見る
-router.get('/:page',(req,res,next)=>{
+router.get('/:page', (req, res, next) => {
     //ログインしてなかったらログインページに飛ばす
-    if(check(req,res))return;
-    const pg=Number(req.params.page);
-    db.serialize(()=>{
+    if (check(req, res)) return;
+    const pg = Number(req.params.page);
+    db.serialize(() => {
         //joinでusersとboardsを合体させる
-        var q='\
+        var q = '\
         SELECT *\
         FROM boards \
         JOIN users ON users.id = boards.accountId \
         ORDER BY boards.createdAt DESC \
         LIMIT ? OFFSET ?\
         ';
-        db.all(q,[pnum,pg*pnum],(err,brds)=>{
-            var data={
-                title:'boards',
-                login:req.session.login,
-                content:brds,
-                page:pg
+        db.all(q, [pnum, pg * pnum], (err, brds) => {
+            var data = {
+                title: 'boards',
+                login: req.session.login,
+                content: brds,
+                page: pg
             }
             console.error(err);
             //console.log(brds);
-            res.render('boards/index',data);
+            res.render('boards/index', data);
         });
     });
 });
 //メッセージフォームの送信処理
-router.post('/add',(req,res,next)=>{
-    if(check(req,res))return;
-    const actId=req.session.login.id;
-    const msg=req.body.msg;
-    db.serialize(()=>{
-        db.run('insert into boards (accountId,message) values (?,?)',actId,msg,(err)=>{
+router.post('/add', (req, res, next) => {
+    if (check(req, res)) return;
+    const actId = req.session.login.id;
+    const msg = req.body.msg;
+    db.serialize(() => {
+        db.run('insert into boards (accountId,message) values (?,?)', actId, msg, (err) => {
             res.redirect('/boards');
         });
     });
 });
 //ユーザーの投稿を見る
-router.get('/home/:user/:id/:page',(req,res,next)=>{
-    if(check(req,res))return;
+router.get('/home/:user/:id/:page', (req, res, next) => {
+    if (check(req, res)) return;
     //+を付けて数値にする
-    const id=+req.params.id;
-    const pg=+req.params.page;
-    db.serialize(()=>{
-        var q='\
+    const id = +req.params.id;
+    const pg = +req.params.page;
+    db.serialize(() => {
+        var q = '\
         SELECT * \
         FROM boards \
         JOIN users ON boards.accountId = users.id \
@@ -89,16 +89,16 @@ router.get('/home/:user/:id/:page',(req,res,next)=>{
         ORDER BY boards.createdAt DESC\
         LIMIT ? OFFSET ? \
         ';
-        db.all(q,[id,pnum,pg*pnum],(err,brds)=>{
-            var data={
-                title:'boards',
-                login:req.session.login,
-                userName:req.params.user,
-                accountId:id,
-                content:brds,
-                page:pg
+        db.all(q, [id, pnum, pg * pnum], (err, brds) => {
+            var data = {
+                title: 'boards',
+                login: req.session.login,
+                userName: req.params.user,
+                accountId: id,
+                content: brds,
+                page: pg
             }
-            res.render('boards/home',data);
+            res.render('boards/home', data);
         });
     });
 });
--- routes/hello.js
+++ routes/hello.js
@@ -1,179 +1,179 @@
 var express = require('express');
 var router = express.Router();
-var WebSocket=require('ws');
+var WebSocket = require('ws');
 
-const sqlite3=require('sqlite3');
-const { check,validationResult } = require('express-validator');
-const db=new sqlite3.Database('mydb.db');
+const sqlite3 = require('sqlite3');
+const { check, validationResult } = require('express-validator');
+const db = new sqlite3.Database('./private/mydb.db');
 
 /* GET home page. */
 // /helloのルートにアクセスしたときの処理
-router.get('/', function(req, res, next) {
-    db.serialize(()=>{
-        db.all('select * from mydata',(err,rows)=>{
-            if(!err){
-                var data={
-                    title:'Hello',
-                    content:rows //取得したレコードデータ
+router.get('/', function (req, res, next) {
+    db.serialize(() => {
+        db.all('select * from mydata', (err, rows) => {
+            if (!err) {
+                var data = {
+                    title: 'Hello',
+                    content: rows //取得したレコードデータ
                 };
-                res.render('hello/index',data);
+                res.render('hello/index', data);
             }
         });
     });
 });
 
-router.get('/add',(req,res,next)=>{
-    var data={
+router.get('/add', (req, res, next) => {
+    var data = {
         title: 'Hello/Add',
         content: '新しいレコードを入力',
-        form: {name:'',mail:'',age:0}
+        form: { name: '', mail: '', age: 0 }
     }
-    res.render('hello/add',data);
+    res.render('hello/add', data);
 });
 
-router.post('/add',[check('name','NAMEは必ず入力してください。').notEmpty(),
-    check('mail','MAILはメールアドレスを入力してください。').isEmail(),
-    check('age','AGEは整数を入力してください').isInt()
-],(req,res,next)=>{
-    const errors=validationResult(req);
+router.post('/add', [check('name', 'NAMEは必ず入力してください。').notEmpty(),
+check('mail', 'MAILはメールアドレスを入力してください。').isEmail(),
+check('age', 'AGEは整数を入力してください').isInt()
+], (req, res, next) => {
+    const errors = validationResult(req);
 
-    if(!errors.isEmpty()){
-        var result='<ul class="text-danger">';
-        var result_arr=errors.array();
-        for(var n in result_arr){
-            result+='<li>'+result_arr[n].msg+'</li>';
+    if (!errors.isEmpty()) {
+        var result = '<ul class="text-danger">';
+        var result_arr = errors.array();
+        for (var n in result_arr) {
+            result += '<li>' + result_arr[n].msg + '</li>';
         }
-        result+='</ul>'
-        var data={
+        result += '</ul>'
+        var data = {
             title: 'Hello/Add',
             content: result,
             form: req.body
         }
-        res.render('hello/add',data);
-    }else{
-        const nm=req.body.name;
-        const ml=req.body.mail;
-        const ag=req.body.age;
-        db.serialize(()=>{
-            db.run('insert into mydata (name,mail,age) values (?,?,?)',nm,ml,ag);
+        res.render('hello/add', data);
+    } else {
+        const nm = req.body.name;
+        const ml = req.body.mail;
+        const ag = req.body.age;
+        db.serialize(() => {
+            db.run('insert into mydata (name,mail,age) values (?,?,?)', nm, ml, ag);
         });
         res.redirect('/hello');
     }
 });
 
-router.get('/show',(req,res,next)=>{
-    const id=req.query.id;
-    db.serialize(()=>{
-        const q='select * from mydata where id = ?';
-        db.get(q,[id],(err,row)=>{
-            if(!err){
-                var data={
+router.get('/show', (req, res, next) => {
+    const id = req.query.id;
+    db.serialize(() => {
+        const q = 'select * from mydata where id = ?';
+        db.get(q, [id], (err, row) => {
+            if (!err) {
+                var data = {
                     title: 'Hello/show',
-                    content: 'id='+id+'のレコード:',
+                    content: 'id=' + id + 'のレコード:',
                     mydata: row
                 }
-                res.render('hello/show',data);
+                res.render('hello/show', data);
             }
         });
     });
 });
 
-router.get('/edit',(req,res,next)=>{
-    const id=req.query.id;
-    db.serialize(()=>{
-        const q='select * from mydata where id = ?';
-        db.get(q,[id],(err,row)=>{
-            if(!err){
-                var data={
+router.get('/edit', (req, res, next) => {
+    const id = req.query.id;
+    db.serialize(() => {
+        const q = 'select * from mydata where id = ?';
+        db.get(q, [id], (err, row) => {
+            if (!err) {
+                var data = {
                     title: 'hello/edit',
-                    content: 'id='+id+'のレコードを編集',
+                    content: 'id=' + id + 'のレコードを編集',
                     mydata: row
                 }
-                res.render('hello/edit',data);
+                res.render('hello/edit', data);
             }
         });
     });
 });
 
-router.post('/edit',(req,res,next)=>{
-    const id=req.body.id;
-    const nm=req.body.name;
-    const ml=req.body.mail;
-    const ag=req.body.age;
-    const q='update mydata set name = ?, mail = ?, age = ? where id = ?';
-    db.serialize(()=>{
-        db.run(q,nm,ml,ag,id);
+router.post('/edit', (req, res, next) => {
+    const id = req.body.id;
+    const nm = req.body.name;
+    const ml = req.body.mail;
+    const ag = req.body.age;
+    const q = 'update mydata set name = ?, mail = ?, age = ? where id = ?';
+    db.serialize(() => {
+        db.run(q, nm, ml, ag, id);
     });
     res.redirect('/hello');
 });
 
-router.get('/delete',(req,res,next)=>{
-    const id=req.query.id;
-    db.serialize(()=>{
-        const q='select * from mydata where id = ?';
-        db.get(q,[id],(err,row)=>{
-            if(!err){
-                var data={
+router.get('/delete', (req, res, next) => {
+    const id = req.query.id;
+    db.serialize(() => {
+        const q = 'select * from mydata where id = ?';
+        db.get(q, [id], (err, row) => {
+            if (!err) {
+                var data = {
                     title: 'hello/delete',
-                    content: 'id='+id+'のレコードを削除',
+                    content: 'id=' + id + 'のレコードを削除',
                     mydata: row
                 }
-                res.render('hello/delete',data);
+                res.render('hello/delete', data);
             }
         });
     });
 });
 
-router.post('/delete',(req,res,next)=>{
-    const id=req.body.id;
-    const q='delete from mydata where id = ?';
-    db.serialize(()=>{
-        db.run(q,[id]);
+router.post('/delete', (req, res, next) => {
+    const id = req.body.id;
+    const q = 'delete from mydata where id = ?';
+    db.serialize(() => {
+        db.run(q, [id]);
     });
     res.redirect('/hello');
 });
 
-router.get('/find',(req,res,next)=>{
-    db.serialize(()=>{
-        const q='select * from mydata';
-        db.all(q,(err,row)=>{
-            if(!err){
-                var data={
+router.get('/find', (req, res, next) => {
+    db.serialize(() => {
+        const q = 'select * from mydata';
+        db.all(q, (err, row) => {
+            if (!err) {
+                var data = {
                     title: 'hello/find',
                     content: '検索条件を入力してください',
                     mydata: row,
                     find: ''
                 }
-                res.render('hello/find',data);
+                res.render('hello/find', data);
             }
         });
     });
 });
 
-router.post('/find',(req,res,next)=>{
-    var find=req.body.find;
-    db.serialize(()=>{
-        var q='select * from mydata where ';
-        db.all(q+find,[],(err,rows)=>{
-            if(!err){
-                var data={
+router.post('/find', (req, res, next) => {
+    var find = req.body.find;
+    db.serialize(() => {
+        var q = 'select * from mydata where ';
+        db.all(q + find, [], (err, rows) => {
+            if (!err) {
+                var data = {
                     title: 'Hello/find',
                     find: find,
-                    content: '検索条件'+find,
+                    content: '検索条件' + find,
                     mydata: rows
                 }
-                res.render('hello/find',data);
+                res.render('hello/find', data);
             }
         });
     });
 });
 
 //websocketの接続テスト
-const server=new WebSocket.Server({port: 3001});
-server.on('connection',(ws)=>{
-    ws.on('message',(msg)=>{
-        console.log('Recieived:'+msg);
-        ws.send('Echo:'+msg);
+const server = new WebSocket.Server({ port: 3001 });
+server.on('connection', (ws) => {
+    ws.on('message', (msg) => {
+        console.log('Recieived:' + msg);
+        ws.send('Echo:' + msg);
     });
     console.log('WebSocket server running on ws://localhost:3001');
 })
